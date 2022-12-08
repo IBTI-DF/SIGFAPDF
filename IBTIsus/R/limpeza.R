90d29a94242b6881c7643eae2cb94d717a8bab88
Leonardo Miranda <leonardobm01@gmail.com>
2022-12-08 13:22:38 -0300

    .

--- IBTIsus/R/limpeza.R
+++ IBTIsus/R/limpeza.R
@@ -200,14 +200,16 @@ limpeza <- function(dados, diretorio){
       dados$COMPETEN <- as.character(dados$COMPETEN)
       dados$COMPETEN <- as.Date(paste(dados$COMPETEN, '01',sep = ''), format = '%Y%m%d')
 
-      dados<-subset(dados, select = -c(REGSAUDE, MICR_REG, DISTRADM, ESFERA_A, RETENCAO, NATUREZA, NIV_HIER, PRONASCI,
-                                      TERCEIRO, CODUFMUN, TPGESTAO, TP_PREST, 
+      dados<-subset(dados, select = -c(REGSAUDE, MICR_REG, DISTRADM, ESFERA_A, RETENCAO, NATUREZA, 
+                                      NIV_HIER, PRONASCI, TERCEIRO, CODUFMUN, TPGESTAO, TP_PREST, 
                                       AP01CV03, AP01CV04, AP02CV03, AP02CV04, AP03CV03, AP03CV04, 
-                                      AP04CV03, AP04CV04, AP05CV02, AP05CV03, AP05CV04, AP05CV05, AP05CV06, 
-                                      AP06CV02, AP06CV03, AP06CV04, AP06CV05, AP06CV06, , AP07CV02, AP07CV03, AP07CV04,
-                                      AP07CV05, AP07CV06, ATEND_PR, GESPRG3M, GESPRG4M, GESPRG6M, PF_PJ, EQBRMEDI,
-                                      CNPJ_MAN, NIV_DEP, VINC_SUS, GESPRG1M, GESPRG2M, GESPRG5M, AP01CV02, AP01CV05, AP01CV06,
-                                      AP02CV01, AP02CV02, AP02CV05, AP02CV06, AP03CV02, AP03CV05, AP03CV06, AP04CV02, AP04CV06,
+                                      AP04CV03, AP04CV04, AP05CV02, AP05CV03, AP05CV04, AP05CV05, 
+                                      AP05CV06, AP06CV02, AP06CV03, AP06CV04, AP06CV05, AP06CV06, 
+                                      AP07CV02, AP07CV03, AP07CV04, AP07CV05, AP07CV06, ATEND_PR, 
+                                      GESPRG3M, GESPRG4M, GESPRG6M, PF_PJ, EQBRMEDI, CNPJ_MAN, 
+                                      NIV_DEP, VINC_SUS, GESPRG1M, GESPRG2M, GESPRG5M, AP01CV02, 
+                                      AP01CV05, AP01CV06, AP02CV01, AP02CV02, AP02CV05, AP02CV06, 
+                                      AP03CV02, AP03CV05, AP03CV06, AP04CV02, AP04CV06,
                                       AP05CV02, ATEND_PR, QUILOMBO, ASSENTAD, ESCOLA, INDIGENA))
     }
 
