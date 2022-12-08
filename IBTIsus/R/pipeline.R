b0f0fd30e8915c810257469bc936fac0f4a4f672
Leonardo Miranda <leonardobm01@gmail.com>
2022-12-08 11:48:34 -0300

    .

--- IBTIsus/R/pipeline.R
+++ IBTIsus/R/pipeline.R
@@ -52,7 +52,7 @@ pipeline <- function(pasta = paste0(getwd(),'/'),periodo = 0, tratamento = T){
 
         setwd(paste0(pasta,'DataSUS/RAW/DBC/',diretorio))
         download.file(paste0(url,strsplit(dat[i], split = ' ')[[1]][length(strsplit(dat[i], split = ' ')[[1]])]), destfile =strsplit(dat[i], split = ' ')[[1]][length(strsplit(dat[i], split = ' ')[[1]])],method = 'curl')
-        dados_brutos <- tryCatch((read.dbc(strsplit(dat[i], split = ' ')[[1]][length(strsplit(dat[i], split = ' ')[[1]])]))
+        dados_brutos <- (read.dbc(strsplit(dat[i], split = ' ')[[1]][length(strsplit(dat[i], split = ' ')[[1]])]))
         if(!file.exists(paste0(pasta,'DataSUS/RAW/CSV/',diretorio))){
           dir.create(paste0(pasta,'DataSUS/RAW/CSV/',diretorio))
         }
